6a9a9b544cac8c685404caadc36d937afce99a25
MrMouga <narnia.assistant@gmail.com>
2023-01-16 20:16:35 +0100

    add function displayBooks: loop array myLibrary and add rows to the table

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let myLibrary = [];
 // array to store all books from the library
 
-function Book(title, author, comment, read)
+function createBook(title, author, comment, read)
 // constructor function to create new books
  {
   this.title=title;
@@ -9,12 +9,48 @@ function Book(title, author, comment, read)
   this.comment=comment;
   this.read=read;
 }
-  
 
 
-function addBookToLibrary(Book)
+function addBookToLibrary(book)
 //function to add new books to the library
  {
-myLibrary.push(Book);
-  
+myLibrary.push(book);
 }
+
+
+//function to loop the array myLibrary and display all books in the table
+
+
+let theHobbit = new createBook("The Hobbit", "J.R.R. Tolkien", "A great book", "Yes");
+
+addBookToLibrary(theHobbit);
+
+let harryPotter = new createBook("Harry Potter", "J.K. Rowling", "A great book", "Yes");
+
+addBookToLibrary(harryPotter);
+
+let gameOfThrones = new createBook("Game of Thrones", "George R.R. Martin", "A great book", "Yes");
+
+addBookToLibrary(gameOfThrones);
+
+let theLordOfTheRings = new createBook("The Lord of the Rings", "J.R.R. Tolkien", "A great book", "Yes");
+
+addBookToLibrary(theLordOfTheRings);
+
+
+function displayBooks() {
+    let table = document.getElementById("myTable");
+    for (let i = 0; i < myLibrary.length; i++) {
+      let row = 
+      `<tr>
+      <td>${myLibrary[i].title}</td>
+      <td>${myLibrary[i].author}</td>
+      <td>${myLibrary[i].comment}</td>
+      <td>${myLibrary[i].read}</td>
+      <td><button class="btn delete-btn">Delete</button></td>
+      </tr>`;
+      table.innerHTML += row;
+    }
+  }
+
+displayBooks();
