6f90c223e1cb610b5ec116cfb68ab090b155ab0d
MrMouga <narnia.assistant@gmail.com>
2023-01-17 13:39:44 +0100

    add javascript function to delete books from table and array

--- script.js
+++ script.js
@@ -18,28 +18,41 @@ myLibrary.push(book);
 }
 
 
-//function to loop the array myLibrary and display all books in the table
 
 
-let theHobbit = new createBook("The Hobbit", "J.R.R. Tolkien", "A great book", "Yes");
+// let theHobbit = new createBook("The Hobbit", "J.R.R. Tolkien", "A great book", "Yes");
 
-addBookToLibrary(theHobbit);
+// addBookToLibrary(theHobbit);
 
-let harryPotter = new createBook("Harry Potter", "J.K. Rowling", "A great book", "Yes");
+// let harryPotter = new createBook("Harry Potter", "J.K. Rowling", "A great book", "Yes");
 
-addBookToLibrary(harryPotter);
+// addBookToLibrary(harryPotter);
 
-let gameOfThrones = new createBook("Game of Thrones", "George R.R. Martin", "A great book", "Yes");
+// let gameOfThrones = new createBook("Game of Thrones", "George R.R. Martin", "A great book", "Yes");
 
-addBookToLibrary(gameOfThrones);
+// addBookToLibrary(gameOfThrones);
 
-let theLordOfTheRings = new createBook("The Lord of the Rings", "J.R.R. Tolkien", "A great book", "Yes");
+// let theLordOfTheRings = new createBook("The Lord of the Rings", "J.R.R. Tolkien", "A great book", "Yes");
 
-addBookToLibrary(theLordOfTheRings);
+// addBookToLibrary(theLordOfTheRings);
 
+let table = document.getElementById("myTable");
 
 function displayBooks() {
-    let table = document.getElementById("myTable");
+    //function to loop the array myLibrary and display all books in the table
+
+    table.replaceChildren();
+    let headerTable=
+    `<tr>
+    <th>Book</th>
+    <th>Author</th>
+    <th>Comment</th>
+    <th>Read</th>
+    <th>Delete</th>
+    </tr>`;
+
+    table.innerHTML += headerTable;
+
     for (let i = 0; i < myLibrary.length; i++) {
       let row = 
       `<tr>
@@ -47,10 +60,56 @@ function displayBooks() {
       <td>${myLibrary[i].author}</td>
       <td>${myLibrary[i].comment}</td>
       <td>${myLibrary[i].read}</td>
-      <td><button class="btn delete-btn">Delete</button></td>
+      <td><button class="delete-btn">Delete</button></td>
       </tr>`;
       table.innerHTML += row;
     }
+
+    deleteBtnListener()
+    
+
   }
 
-displayBooks();
+
+function submitBook(){
+    alert("Book added to the library");
+  let bookValue=document.getElementById("bookField").value;
+  let authorValue=document.getElementById("authorField").value;
+  let commentValue=document.getElementById("commentField").value;
+  let readValue=document.getElementById("readField").value;
+
+  let newBook = new createBook(bookValue, authorValue, commentValue, readValue);
+  addBookToLibrary(newBook);
+  displayBooks();
+}
+
+document.getElementById("myForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  submitBook(); 
+});
+
+
+
+function deleteBtnListener(){
+let deleteBtns = document.querySelectorAll(".delete-btn");
+    deleteBtns.forEach((btn) => {btn.addEventListener("click", (e) => {
+
+        let bookDeleted = e.target.parentElement.parentElement;
+        deleteBook(bookDeleted)
+    })});}
+
+
+
+
+function deleteBook(bookDeleted){
+   
+        for (let i = 0; i < myLibrary.length; i++) {
+            if (myLibrary[i].title === bookDeleted.children[0].textContent) {
+                myLibrary.splice(i, 1);
+            }}
+
+        bookDeleted.remove();
+      }
+
+
+
